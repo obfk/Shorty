99382b50a4860caa7fadd99da4e6ead89a059967
Devin Brown <devin.g.brown@gmail.com>
2015-06-10 16:43:20 -0600

    made a browser

--- Shorty/ViewController.swift
+++ Shorty/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var urlField: UITextField!
+    @IBOutlet var webView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loadLocation ( AnyObject ) {
+        var urlText = urlField.text
+
+        if !urlText.hasPrefix("http:") && !urlText.hasPrefix("https:") {
+            if !urlText.hasPrefix("//") {
+                urlText = "//" + urlText
+            }
+            urlText = "http:" + urlText
+        }
+
+        let url = NSURL(string: urlText)
+        webView.loadRequest(NSURLRequest(URL: url!))
+
+    }
 
 }
 
